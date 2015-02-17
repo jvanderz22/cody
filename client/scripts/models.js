9b1b5bea73545b92e58173fbcd5166c562d46db8
Jordan VanderZwaag <jvanderzwaag@enova.com>
2015-02-17 00:56:33 -0600

    minor corresponding front end change

--- client/scripts/models.js
+++ client/scripts/models.js
@@ -3,20 +3,14 @@ app.factory('Image', function($http, $sce) {
     getAllImages: function() {
       return $http.get('api/images').
         then(function(response) {
-          images = []
-          allImages = response.data
-          for (key in allImages) {
-            images = images.concat(allImages[key])
-          }
-          return images
+          return response.data['images']
         })
     },
 
     getCategoryImages: function(category) {
       return $http.get('api/images?category=' + category).
         then(function(response) {
-          images = response.data[category]
-          return images
+          return response.data['images']
         })
     }
   }
